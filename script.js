60bff20a138ddeae41f174613953a530f0293815
Islam-Ahmadiyyat <kamranahmedi1272@gmail.com>
2025-01-13 23:53:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".read-btn").forEach(button => {
+/* document.querySelectorAll(".read-btn").forEach(button => {
   button.addEventListener("click", function () {
     const bookFileName = this.parentElement.getAttribute("data-book");
     const pdfWindow = window.open("", "_blank");
@@ -6,4 +6,12 @@ document.querySelectorAll(".read-btn").forEach(button => {
       `<iframe src="${bookFileName}" width="100%" height="100%" style="border:none;"></iframe>`
     );
   });
-});
+}); */
+
+document.querySelectorAll(".read-btn").forEach(button => {
+      button.addEventListener("click", function () {
+        const bookFileName = this.parentElement.getAttribute("data-book");
+        const viewerURL = `viewer.html?file=${encodeURIComponent(bookFileName)}`;
+        window.open(viewerURL, "_blank"); // Open the viewer in a new tab
+      });
+    });
